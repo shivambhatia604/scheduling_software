36d5cf94185af1242a43dc6684d64cf55c00d8e2
Shivam Bhatia <shivambhatia@outlook.com>
2024-11-26 02:07:47 +0530

    invalidating stale  query

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 import { useState, useEffect } from "react";
 import Calendar from "@/ui/atoms/calendar";
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
-import { EllipsisHorizontalIcon } from "@heroicons/react/20/solid";
 import MeetingsCard from "../components/MeetingsCard";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { convertDateToStartUTCDateTime } from "@/lib/helpers";
 import Loader from "../components/loader";
 import { useNotification } from "@/ui/context/NotificationContext";
@@ -12,6 +10,7 @@ import { useNotification } from "@/ui/context/NotificationContext";
 export default function Events() {
   const [date, setDate] = useState(new Date());
   const { showNotification } = useNotification();
+  const queryClient = useQueryClient();
 
   const { isPending, isError, data, error } = useQuery({
     queryKey: ["meetingsByDate"],
@@ -22,14 +21,17 @@ export default function Events() {
       return await data.json();
     },
   });
-  const handleDateSelect = (date: {
+  useEffect(() => {
+    queryClient.invalidateQueries({ queryKey: ["meetingsByDate"] });
+  }, [date]);
+  const handleDateSelect = (selectedDate: {
     date: Date;
     isCurrentMonth: boolean;
     isToday: boolean;
   }) => {
-    setDate(date.date);
+    setDate(selectedDate.date);
   };
-  console.log(data);
+
   if (isError) {
     showNotification("Error Occured", error.message, "error");
   }
@@ -40,11 +42,7 @@ export default function Events() {
 
   return (
     <div className="flex flex-col md:flex-row justify-between md:items-start md:gap-x-16">
-      <MeetingsCard
-        className="grow"
-        isEditable
-        meetings={data.data}
-      />
+      <MeetingsCard className="grow" isEditable meetings={data.data} />
 
       <div className="grow">
         <Calendar selectedDate={date} handleDateSelect={handleDateSelect} />
